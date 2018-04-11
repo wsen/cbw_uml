d39200a4d8fd8ee4a128ca7a67d40bd80dcb9508
Werner Hillenbrand <1767801@cbw.intern>
2018-04-11 10:36:58 +0200

    first commit

---
+++ src/uml/huxtlipotzli.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uml;
+
+/**
+ *
+ * @author 1767801
+ */
+public class huxtlipotzli {
+    
+}
